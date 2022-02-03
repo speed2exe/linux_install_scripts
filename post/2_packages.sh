21189195a541287649d67ba13871b78e3e4ac17e
Fu Zi Xiang <speed2exe@live.com.sg>
2022-02-03 14:20:54 +0800

    added xclip

--- post/2_packages.sh
+++ post/2_packages.sh
@@ -1,4 +1,4 @@
 pacman -Syu xorg bspwm rofi picom thunar pinta maim nitrogen \
-    kitty sxhkd xorg-xinit gtk4 dunst xbindkeys xdg-utils
+    kitty sxhkd xorg-xinit gtk4 dunst xbindkeys xdg-utils xclip
 
 
