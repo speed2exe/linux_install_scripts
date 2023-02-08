cb1b56881e0ffd1ac230f094cfb35e4ae4478a95
Fu Zi Xiang <speed@Fus-Laptop.local>
2023-02-08 15:10:52 +0800

    user: added ansifilter

--- user.sh
+++ user.sh
@@ -27,7 +27,7 @@ cd paru-bin/
 makepkg -si
 sleep 5
 paru -Syu timeshift timeshift-autosnap zramd microsoft-edge-stable-bin \
-    polybar backlight_control nvim-packer-git stderred-git \
+    polybar backlight_control nvim-packer-git stderred-git ansifilter \
     --noconfirm
 cd ~
 
