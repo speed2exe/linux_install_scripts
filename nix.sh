1d2495b17cbce2263cbf7f1ae4a59c1aa5e406fe
Fu Zi Xiang <speed2exe@live.com.sg>
2024-01-22 23:43:00 +0800

    initial btrfs setup

--- nix.sh
+++ nix.sh
@@ -2,4 +2,34 @@
 #
 # curl https://raw.githubusercontent.com/speed2exe/linux_install_scripts/main/nix.sh | sudo bash
 
-echo hello
+boot=$1
+volume=$2
+
+if test -z "$boot"
+then
+    echo "first arg should have boot partition"
+    exit 1
+fi
+
+if test -z "$volume"
+then
+    echo "second arg should have root partition"
+    exit 1
+fi
+
+mkfs.vfat $boot
+mkfs.btrfs -f $volume
+
+mount $volume /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@nix
+umount /mnt
+
+mount -o compress=zstd,subvol=@ $volume /mnt
+mkdir -p /mnt/{boot,home,nix}
+mount -o compress=zstd,subvol=@home $volume /mnt/home
+mount -o compress=zstd,subvol=@nix $volume /mnt/nix
+mount $boot /mnt/boot
+
+nixos-generate-config --root /mnt
